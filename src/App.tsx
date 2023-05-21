69afa614c587d29d1361bc7ba368488ffb7ee6f8
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 16:08:35 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import {Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+
 
 function App() {
-  return <h1>Hi</h1>
+  return (
+  <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/store" element={<Store />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+  </Container>
+  )
 }
 
 export default App
