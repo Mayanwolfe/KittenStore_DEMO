7dac13df68af8163205de1d1d6e0e5a0793bc55e
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 19:59:30 -0400

    added context to StoreItem

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 type StoreItemProps = {
   id: number
@@ -10,7 +11,8 @@ type StoreItemProps = {
 
 
 export function StoreItem({id, name, price, imgUrl} : StoreItemProps) {
-  const quantity = 1
+  const {getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart} = useShoppingCart()
+  const quantity = getItemQuantity(id)
   return (
   <Card className="h-100">
     <Card.Img variant = "top" src = {imgUrl} height="200px" style={{objectFit: "cover"}} />
@@ -20,16 +22,16 @@ export function StoreItem({id, name, price, imgUrl} : StoreItemProps) {
         <span className="ms-2 text-muted">{formatCurrency(price)}</span>
       </Card.Title>
       <div className="mt-auto">
-        {quantity === 0 ? (<Button className="w-100">+ Add to Cart</Button> ): (
+        {quantity === 0 ? (<Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add to Cart</Button> ): (
           <div className="d-flex align-items-center flex-column" style={{gap: ".5rem"}}>
             <div className="d-flex align-items-center justify-content-center" style={{gap: ".5rem"}}>
-              <Button>-</Button>
+              <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
               <div>
               <span className="fs-3">{quantity}</span> in cart
               </div>
-              <Button>+</Button>
+              <Button onClick={() => increaseCartQuantity(id)}>+</Button>
             </div>
-            <Button variant="danger" size="sm">Remove</Button>
+            <Button onClick={() => removeFromCart(id)} variant="danger" size="sm">Remove</Button>
           </div>
         )}
       </div>
