cd5e4636e8add1e8d0ada6c1b28f634f11588e8f
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 17:59:05 -0400

    started store cards

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,23 @@
+import { Card } from "react-bootstrap"
+
+type StoreItemProps = {
+  id: number
+  name: string
+  price: number
+  imgUrl: string
+}
+
+
+export function StoreItem({id, name, price, imgUrl} : StoreItemProps) {
+  return (
+  <Card>
+    <Card.Img variant = "top" src = {imgUrl} height="200px" style={{objectFit: "cover"}} />
+    <Card.Body className="d-flex flex-column">
+      <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+        <span className="fs-2">{name}</span >
+        <span className="ms-2 text-muted">{price}</span>
+      </Card.Title>
+    </Card.Body>
+  </Card>
+  )
+}
