83c05181877aa6e1977a3ba49af3fe60a34da5c9
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 16:48:30 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import {Navbar} from "./components/Navbar"
 
 
 function App() {
   return (
+  <>
+  <Navbar />
   <Container className="mb-4">
     <Routes>
       <Route path="/" element={<Home />}/>
@@ -14,6 +17,7 @@ function App() {
       <Route path="/about" element={<About />}/>
     </Routes>
   </Container>
+  </>
   )
 }
 
