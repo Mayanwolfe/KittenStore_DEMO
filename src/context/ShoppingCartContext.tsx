8e8f93dd80b6f9fab7abf005c50a5fd9a0bad793
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 19:37:41 -0400

    finished context logic

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext } from "react"
+import { ReactNode, createContext, useContext, useState } from "react"
 
 type ShoppingCartProviderProps = {
   children: ReactNode
@@ -11,6 +11,11 @@ type ShoppingCartContext = {
   removeFromCart: (id: number) => void
 }
 
+type CartItem = {
+  id: number
+  quantity: number
+}
+
 const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 
@@ -21,8 +26,52 @@ export function useShoppingCart() {
 
 //This is the radio station that broadcasts the context
 export function ShoppingCartProvider({children}  : ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function getItemQuantity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if(currItems.find(item => item.id === id) == null){
+        return [...currItems, {id, quantity: 1}]
+      } else {
+        return currItems.map(item => {
+          if (item.id===id) {
+            return {...item, quantity: item.quantity + 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if(currItems.find(item => item.id === id)?.quantity === 1){
+        return currItems.filter(item => item.id !== id)
+      } else {
+        return currItems.map(item => {
+          if (item.id===id) {
+            return {...item, quantity: item.quantity - 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id)
+    })
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
       {children}
     </ShoppingCartContext.Provider>
   )
