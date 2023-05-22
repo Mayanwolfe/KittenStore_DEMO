38cb1d46f9d06c39355096a43eeb28956a448d86
Claire Bourdon <cebourdon625@gmail.com>
2023-05-21 21:07:28 -0400

    images in cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,15 +1,23 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
 
 type ShoppingCartProps = {
   isOpen: boolean
 }
 
 export function ShoppingCart({isOpen} : ShoppingCartProps) {
-  const {closeCart} = useShoppingCart()
+  const {closeCart, cartItems} = useShoppingCart()
   return <Offcanvas show={isOpen} onHide={closeCart} placement="end">
     <Offcanvas.Header closeButton>
       <Offcanvas.Title>Cart</Offcanvas.Title>
     </Offcanvas.Header>
+    <Offcanvas.Body>
+      <Stack gap={3}>
+        {cartItems.map(item => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </Stack>
+    </Offcanvas.Body>
   </Offcanvas>
 }
